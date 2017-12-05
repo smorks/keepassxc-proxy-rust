805fb73c8d1c4d55aa6e0d1ad75648645927bbdb
Andy Brandt <andy@brandt.tech>
2017-12-05 15:08:07 -0600

    use multiple threads, simplify proxy socket

--- src/main.rs
+++ src/main.rs
@@ -2,69 +2,137 @@ extern crate byteorder;
 #[cfg(windows)]
 extern crate named_pipe;
 
+use std::fs::File;
 use std::io::{self, Read, Write};
+use std::ops::DerefMut;
 use std::thread;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::channel;
 use byteorder::{ByteOrder, NativeEndian, WriteBytesExt};
 
 mod proxy_socket;
 
-use proxy_socket::ProxySocket;
-
-const BUFFER_SIZE: u32 = 1024 * 16;	 // 1024 ^ 2 is the maximum
+const BUFFER_SIZE: u32 = 1024 * 16; // 1024 ^ 2 is the maximum
 
 fn valid_length(length: u32) -> bool {
 	return length > 0 && length <= BUFFER_SIZE;
 }
 
-fn read_header() -> u32 {
+fn read_stdin() -> Option<Vec<u8>> {
 	let stdin = io::stdin();
 	let mut buf = vec![0; 4];
 	let mut handle = stdin.lock();
 
 	handle.read_exact(&mut buf).unwrap();
-	NativeEndian::read_u32(&buf)
-}
+	let len = NativeEndian::read_u32(&buf);
 
-fn read_body<T: Read + Write>(length: u32, socket: &mut ProxySocket<T>) {
-	let mut buffer = vec![0; length as usize];
-	let stdin = io::stdin();
-	let mut handle = stdin.lock();
+	let mut buffer = vec![0; len as usize];
 
 	if let Ok(_) = handle.read_exact(&mut buffer) {
-		if valid_length(length) {
-			socket.write(&buffer).unwrap();
-			socket.flush().unwrap();
-			read_response(socket);
+		if valid_length(len) {
+			return Some(buffer);
 		}
 	}
+	None
 }
 
-fn read_response<T: Read>(socket: &mut ProxySocket<T>) {
-	let mut buf = vec![0; BUFFER_SIZE as usize];
-	if let Ok(len) = socket.read(&mut buf) {
-		write_response(&buf[0..len]);
-	}
-}
-
-fn write_response(buf: &[u8]) {
+fn write_stdout(buf: &[u8]) -> io::Result<()> {
 	let stdout = io::stdout();
 	let mut out = stdout.lock();
 
-	out.write_u32::<NativeEndian>(buf.len() as u32).unwrap();
-	out.write(buf).unwrap();
-	out.flush().unwrap();
+	out.write_u32::<NativeEndian>(buf.len() as u32)?;
+	out.write(buf)?;
+	out.flush()?;
+	Ok(())
+}
+
+fn read_socket<T: Read>(socket: &mut T) -> Option<Vec<u8>> {
+	let mut buf = vec![0; BUFFER_SIZE as usize];
+	if let Ok(len) = socket.read(&mut buf) {
+		return Some(Vec::from(&buf[0..len]));
+	}
+	None
+}
+
+fn write_socket<T: Write>(socket: &mut T, data: &[u8]) -> io::Result<()> {
+	socket.write(data)?;
+	socket.flush()?;
+	Ok(())
 }
 
 fn main() {
-	let mut socket = proxy_socket::connect().unwrap();
+	let mut log = File::create("proxy.log").unwrap();
 
-	// Start thread for user input reading
-	let ui = thread::spawn(move || {
+	let socket = proxy_socket::connect().unwrap();
+	let socket_counter = Arc::new(Mutex::new(socket));
+
+	let mut threads = vec![];
+
+	let (stdin_tx, stdin_rx) = channel::<Vec<u8>>();
+	let (stdout_tx, stdout_rx) = channel::<Vec<u8>>();
+	let (sw_tx, sw_rx) = channel::<u8>();
+	let (log_tx, log_rx) = channel::<(&str, Vec<u8>)>();
+	let log_tx2 = log_tx.clone();
+
+	// stdin
+	threads.push(thread::spawn(move || {
 		loop {
-			let length = read_header();
-			read_body(length, &mut socket);
+			if let Some(data) = read_stdin() {
+				log_tx.send(("\nread_stdin:\n", data.clone())).unwrap();
+				stdin_tx.send(data).unwrap();
+			}
 		}
-	});
+	}));
 
-	let _ui_res = ui.join().unwrap();
+	// stdout
+	threads.push(thread::spawn(move || {
+		loop {
+			if let Ok(data) = stdout_rx.recv() {
+				write_stdout(&data).unwrap();
+			}
+		}
+	}));
+
+	// socket write
+	let socket_write = Arc::clone(&socket_counter);
+	threads.push(thread::spawn(move || {
+		loop {
+			if let Ok(data) = stdin_rx.recv() {
+				let mut s = socket_write.lock().unwrap();
+				if let Ok(_) = write_socket(s.deref_mut(), &data) {
+					sw_tx.send(0).unwrap();
+				}
+			}
+		}
+	}));
+
+	// socket read
+	let socket_read = Arc::clone(&socket_counter);
+	threads.push(thread::spawn(move || {
+		loop {
+			if let Ok(_) = sw_rx.recv() {
+				let mut s = socket_read.lock().unwrap();
+				if let Some(data) = read_socket(s.deref_mut()) {
+					log_tx2.send(("\nread_socket:\n", data.clone())).unwrap();
+					stdout_tx.send(data).unwrap();
+				}
+			}
+		}
+	}));
+
+	// logging
+	threads.push(thread::spawn(move || {
+		loop {
+			if let Ok(data) = log_rx.recv() {
+				log.write_all(data.0.as_bytes()).unwrap();
+				log.write_all(&data.1).unwrap();
+				log.write_all(b"\n").unwrap();
+				log.flush().unwrap();
+			}
+		}
+	}));
+
+	for t in threads {
+		t.join().unwrap();
+	}
 }
--- src/proxy_socket.rs
+++ src/proxy_socket.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io;
 
 #[cfg(not(windows))]
 use std::os::unix::net::UnixStream;
@@ -7,36 +7,16 @@ use std::os::unix::net::UnixStream;
 #[cfg(windows)]
 use named_pipe::PipeClient;
 
-pub struct ProxySocket<T> {
-	inner: T,
-}
-
-impl<R: Read> Read for ProxySocket<R> {
-	fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-		self.inner.read(buf)
-	}
-}
-
-impl<W: Write> Write for ProxySocket<W> {
-	fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-		self.inner.write(buf)
-	}
-
-	fn flush(&mut self) -> io::Result<()> {
-		self.inner.flush()
-	}
-}
-
 #[cfg(windows)]
-pub fn connect() -> io::Result<ProxySocket<PipeClient>> {
-	let temp_path = env::var("TEMP").unwrap();
-	let pipe_name = format!("\\\\.\\pipe\\{}\\kpxc_server", temp_path);
+pub fn connect() -> io::Result<PipeClient> {
+	let username = env::var("USERNAME").unwrap();
+	let pipe_name = format!("\\\\.\\pipe\\keepassxc\\{}\\kpxc_server", username);
 	let client = PipeClient::connect(pipe_name)?;
-	Ok(ProxySocket { inner: client })
+	Ok(client)
 }
 
 #[cfg(not(windows))]
-pub fn connect() -> io::Result<ProxySocket<UnixStream>> {
+pub fn connect() -> io::Result<UnixStream> {
 	use std::time::Duration;
 
 	let socket_name = "kpxc_server";
@@ -49,5 +29,5 @@ pub fn connect() -> io::Result<ProxySocket<UnixStream>> {
 	let s = UnixStream::connect(socket)?;
 	let timeout: Option<Duration> = Some(Duration::from_secs(1));
 	s.set_read_timeout(timeout)?;
-	Ok(ProxySocket { inner: s })
+	Ok(s)
 }
